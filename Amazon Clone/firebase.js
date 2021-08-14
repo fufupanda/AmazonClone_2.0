8d51554ea5b1f4f87aac41022e76cb611563a8fb
fufupanda <85630573+fufupanda@users.noreply.github.com>
2021-08-15 00:58:06 +0530

    Add files via upload

---
+++ Amazon Clone/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAN49YAIEjGXpMCxya-u8PJZCTCV3gee4k",
+    authDomain: "clone-304d1.firebaseapp.com",
+    projectId: "clone-304d1",
+    storageBucket: "clone-304d1.appspot.com",
+    messagingSenderId: "510639976035",
+    appId: "1:510639976035:web:16c868a9b2cc36785d5256"
+  };
+   // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
+
+   var db = firebase.firestore();
